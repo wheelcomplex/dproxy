a99036b5fd1716781a055a35b3287c0eeb7f4630
David Mortals <wheelcomplex@users.noreply.github.com>
2019-08-20 20:09:49 +1200

    initial main go

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+
+	"github.com/valyala/fasthttp"
+	proxy "github.com/wheelcomplex/fasthttp-reverse-proxy"
+)
+
+var (
+	proxyServer = proxy.NewReverseProxy("localhost:8080")
+)
+
+// ProxyHandler ... fasthttp.RequestHandler func
+func ProxyHandler(ctx *fasthttp.RequestCtx) {
+	// all proxy to localhost
+	proxyServer.ServeHTTP(ctx)
+}
+
+func main() {
+	if err := fasthttp.ListenAndServe(":8081", ProxyHandler); err != nil {
+		log.Fatal(err)
+	}
+}
